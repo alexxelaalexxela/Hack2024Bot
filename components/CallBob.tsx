2a39d4199dd7377b8abc967239198c563e396a0a
Alexandre <alexandre.clin@orange.fr>
2024-03-22 00:18:40 +0100

    phoneMatch

--- components/CallBob.tsx
+++ components/CallBob.tsx
@@ -21,8 +21,8 @@ export default function CallBob() {
   } = useCallManager();
   return (
     <div className="flex lg:flex-row lg:items-center lg:justify-center xxs:h-full flex-col items-center justify-end overflow-auto">
-      <div className="bg-[url(../public/assets/Bob.gif)] lg:h-[500px] lg:w-[500px] xxs:h-0 w-full bg-no-repeat bg-contain bg-center"></div>
-      <div className="flex justify-center flex-col items-center lg:w-[calc(100%-600px)] w-full xxs:h-full">
+      <div className="lg:bg-[url(../public/assets/Bob.gif)] bg-cover lg:h-[500px] lg:w-[500px] w-full bg-no-repeat bg-contain bg-center xxs:bg-[url(../public/assets/Bob.gif)] xxs:bg-cover xxs:h-[1000px] xxs:pt-30"></div>
+      <div className="flex justify-center flex-col items-center lg:w-[calc(100%-600px)] w-full xxs:h-full z-10 xxs:pt-8">
         <MessageBox message={messages[messages.length - 1].message} />
         <div className="mb-8 flex justify-center flex-col items-center">
           <TalkButton
