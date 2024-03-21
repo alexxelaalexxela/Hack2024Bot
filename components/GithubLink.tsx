a0ebffefecc2e2bd6ef83439eb293132dd20c06e
Alexandre <alexandre.clin@orange.fr>
2024-03-21 23:23:11 +0100

    flag

--- components/GithubLink.tsx
+++ components/GithubLink.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import GithubLogo from '../public/assets/github.svg';
+
+
 export function GithubLink() {
-  const handleClick = () => {
-    window?.open('https://github.com/shihui-huang/call-bob', '_blank')?.focus();
-  };
+
   return (
     <div className="mx-1">
+      <div className="bg-[url(../public/assets/flag.png)] lg:h-[100px] lg:w-[100px] xxs:h-0 w-full bg-no-repeat bg-contain bg-center"></div>
     </div>
   );
 }
